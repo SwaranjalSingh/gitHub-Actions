21c9df4a975b06301346cad6db78fa9fc95a1d24
SwaranjalSingh <swaranjal3905@gmail.com>
2023-03-14 15:09:56 +0530

    InputCommit

--- UserInput/Input.py
+++ UserInput/Input.py
@@ -1,10 +1,5 @@
 import sys
 
-def userInput(Name, Age):
-    return " "
-
 Name = sys.argv[1]
 Age = sys.argv[2]
-# c = sys.argv[3]
-
-print(f'user Name is: {Name} and age is : {Age}{userInput(Name, Age)}')
+print(f'user Name is: {Name} and age is : {Age}')
